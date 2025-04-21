519bbbfa4962abf3df184dae711165d91b3d59fc
xbatm <dbustos28@amarillocollege.com>
2025-04-20 22:38:14 -0500

    Add project files.

---
+++ Week 2-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int F, C;
+            Console.WriteLine("Welcome to Temperature Converter");// Calcucaltor
+            Console.WriteLine("Enter the temperature: "); // enter the tempature
+            F = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(C = (F-32) * 5/9); // get total for Celcius
+            Console.WriteLine($"{F} Fernite to {C} Celsius"); // read result
+
+            Console.ReadLine(); // readline
+        }
+    }
+}
